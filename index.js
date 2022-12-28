a09ba6e8f4c9468efa7d940fa5af18df65cc087f
kunal2144 <jkunal2144@gmail.com>
2022-12-28 18:27:12 +0530

    Uploading all files
    index.html, index.css, index.js

---
+++ index.js
@@ -0,0 +1,31 @@
+const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
+"/"];
+
+let passwordContent = document.getElementsByClassName("password-content")
+let passwordFields = document.getElementsByClassName("password");
+console.log(passwordContent)
+
+for(let i = 0; i < passwordFields.length; i++) {
+    passwordFields[i].style.cursor = "pointer"
+    passwordFields[i].onclick = function() {
+    navigator.clipboard.writeText(this.textContent)
+}
+}
+
+
+
+function generatePassword() {
+    let passwordOneContent = ""
+    let passwordTwoContent = ""
+    
+    for(let i = 0; i < 15; i++)
+    {
+        passwordOneContent += characters[Math.floor(Math.random() * characters.length)]
+        passwordTwoContent += characters[Math.floor(Math.random() * characters.length)]
+    }
+
+    passwordContent[0].textContent = passwordOneContent
+    passwordContent[1].textContent = passwordTwoContent
+}
+
+
