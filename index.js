1274528c5147ab94fc7b9d759878124b4ed66562
Kunal N Jain <69496316+kunal2144@users.noreply.github.com>
2023-05-23 10:21:44 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,30 +1,118 @@
-const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
-"/"];
+const characters = [
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "I",
+    "J",
+    "K",
+    "L",
+    "M",
+    "N",
+    "O",
+    "P",
+    "Q",
+    "R",
+    "S",
+    "T",
+    "U",
+    "V",
+    "W",
+    "X",
+    "Y",
+    "Z",
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
+    "0",
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "~",
+    "`",
+    "!",
+    "@",
+    "#",
+    "$",
+    "%",
+    "^",
+    "&",
+    "*",
+    "(",
+    ")",
+    "_",
+    "-",
+    "+",
+    "=",
+    "{",
+    "[",
+    "}",
+    "]",
+    ",",
+    "|",
+    ":",
+    ";",
+    "<",
+    ">",
+    ".",
+    "?",
+    "/",
+]
 
 let passwordContent = document.getElementsByClassName("password-content")
-let passwordFields = document.getElementsByClassName("password");
+let passwordFields = document.getElementsByClassName("password")
 
-for(let i = 0; i < passwordFields.length; i++) {
+for (let i = 0; i < passwordFields.length; i++) {
     passwordFields[i].style.cursor = "pointer"
-    passwordFields[i].onclick = function() {
-    navigator.clipboard.writeText(this.textContent)
+    passwordFields[i].onclick = function () {
+        navigator.clipboard.writeText(this.textContent)
+    }
 }
-}
-
-
 
 function generatePassword() {
     let passwordOneContent = ""
     let passwordTwoContent = ""
-    
-    for(let i = 0; i < 15; i++)
-    {
-        passwordOneContent += characters[Math.floor(Math.random() * characters.length)]
-        passwordTwoContent += characters[Math.floor(Math.random() * characters.length)]
+
+    for (let i = 0; i < 15; i++) {
+        passwordOneContent +=
+            characters[Math.floor(Math.random() * characters.length)]
+        passwordTwoContent +=
+            characters[Math.floor(Math.random() * characters.length)]
     }
 
     passwordContent[0].textContent = passwordOneContent
     passwordContent[1].textContent = passwordTwoContent
 }
-
-
